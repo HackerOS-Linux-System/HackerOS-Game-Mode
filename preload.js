d1fa0be56f4853c727468c60d55bfe8babacc59f
michal92299 <chudybyczek2137@gmail.com>
2025-10-17 09:35:34 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    onUpdateStats: (callback) => ipcRenderer.on('update-stats', callback)
+    onUpdateStats: (callback) => ipcRenderer.on('update-stats', callback),
+                                onGetRects: (callback) => ipcRenderer.on('get-rects', callback),
+                                onStopRecording: (callback) => ipcRenderer.on('stop-recording', callback),
+                                send: (channel) => ipcRenderer.send(channel),
+                                desktopCapturer: require('electron').desktopCapturer
 });
